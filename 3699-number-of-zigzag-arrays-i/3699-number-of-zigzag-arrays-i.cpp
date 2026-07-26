a0a2587879c75d2d49bdf49e92e634a22dbdb40e
Saptarshi Sau <saptarshisau09@gmail.com>
2026-07-26 23:38:59 +0530

    Time: 1119 ms (13.94%), Space: 85.9 MB (27.23%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -5,34 +5,46 @@ public:
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
         ll res = 0LL;
-        
-        vector<vector<int>> next_dp(m + 1, vector<int>(2, 1));
-        vector<vector<int>> curr_dp(m + 1, vector<int>(2, 0));
-        vector<ll> sum_inc(m + 1, 0);
-        vector<ll> sum_dec(m + 1, 0);
-
-        for (int i = n - 1; i >= 1; i--) {
+        // vector<vector<vector<int>>> dp(
+        //     n + 1, vector<vector<int>>(m + 1, vector<int>(2, 0)));
+        vector<ll> sum_inc(m + 1, 0), sum_dec(m + 1, 0);
+        ll dp[2001][2001][2]; //compile time allocation, takes close to no time
+        for (int i = 0; i <= m; i++) {
+            dp[n][i][0] = 1;
+            dp[n][i][1] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
             for (int start = 1; start <= m; start++) {
-                sum_inc[start] = (sum_inc[start - 1] + next_dp[start][0]) % mod;
-                sum_dec[start] = (sum_dec[start - 1] + next_dp[start][1]) % mod;
+                sum_inc[start] =
+                    (sum_inc[start - 1] + dp[i + 1][start][0]) % mod;
+                sum_dec[start] =
+                    (sum_dec[start - 1] + dp[i + 1][start][1]) % mod;
             }
-            
             for (int prevVal = m; prevVal >= 1; prevVal--) {
-                curr_dp[prevVal][1] = (sum_inc[m] - sum_inc[prevVal] + mod) % mod;
-                curr_dp[prevVal][0] = sum_dec[prevVal - 1];
+
+                // for (int start = prevVal + 1; start <= m; start++) {
+                //     dp[i][prevVal][1] = (dp[i][prevVal][1] + dp[i +
+                //     1][start][0]) % mod;
+                // }
+                dp[i][prevVal][1] = (sum_inc[m] - sum_inc[prevVal] + mod) % mod;
+
+                // for (int start = 1; start < prevVal; start++) {
+                //     dp[i][prevVal][0] = (dp[i][prevVal][0] + dp[i +
+                //     1][start][1]) % mod;
+                // }
+
+                dp[i][prevVal][0] = (sum_dec[prevVal - 1]);
             }
-            
-            next_dp = curr_dp;
         }
 
         for (int startVal = 1; startVal <= m; startVal++) {
-            res = (res + next_dp[startVal][1]) % mod;
-            res = (res + next_dp[startVal][0]) % mod;
+            res = (res + dp[1][startVal][1]) % mod;
+            res = (res + dp[1][startVal][0]) % mod;
         }
-        
         return res;
     }
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
