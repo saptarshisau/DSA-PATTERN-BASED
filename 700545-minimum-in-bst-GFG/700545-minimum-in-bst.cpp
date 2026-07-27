3497ebddff33d0f561f50fb7b88e5200d0a7a385
Saptarshi Sau <saptarshisau09@gmail.com>
2026-07-27 12:36:24 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 700545-minimum-in-bst-GFG/700545-minimum-in-bst.cpp
@@ -0,0 +1,32 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // code here
+        int x=1e9;
+        Node* curr=root;
+        while(curr){
+            x=min(x,curr->data);
+            curr=curr->left;
+        }
+        return x;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
