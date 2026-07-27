1fb2066dfb1d7c212e5fcd9ef447123b28fa5066
Saptarshi Sau <saptarshisau09@gmail.com>
2026-07-27 11:19:42 +0530

    Time: 14 ms (34.71%), Space: 11.8 MB (61.46%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int minNumberOfFrogs(string croakOfFrogs) {
-        int hash[26]={0};
-        string req="croak";
-        for(auto &it: croakOfFrogs){
-            hash[it-'a']++;
-            if(hash['c'-'a']<hash['r'-'a'] || hash['r'-'a']<hash['o'-'a'] || hash['o'-'a']<hash['a'-'a'] || hash['a'-'a']<hash['k'-'a']) return -1;
+        int hash[26] = {0};
+        string req = "croak";
+        for (auto& it : croakOfFrogs) {
+            hash[it - 'a']++;
+            int idx = req.find(it);
+
+            if (idx > 0 && hash[req[idx] - 'a'] > hash[req[idx - 1] - 'a']) {
+                return -1;
+            }
         }
-        int count=hash['c'-'a'];
-        for(auto &it:req){
-            if(count!=hash[it-'a']) return -1;
+        int count = hash['c' - 'a'];
+        for (auto& it : req) {
+            if (count != hash[it - 'a'])
+                return -1;
         }
-        int cnt_c=0,maxi=0;
-        for(auto &it:croakOfFrogs){
-            if(it=='c') cnt_c++;
-            if(it=='k') cnt_c--;
-            maxi=max(maxi,cnt_c);
+        int cnt_c = 0, maxi = 0;
+        for (auto& it : croakOfFrogs) {
+            if (it == 'c')
+                cnt_c++;
+            if (it == 'k')
+                cnt_c--;
+            maxi = max(maxi, cnt_c);
         }
         return maxi;
     }
