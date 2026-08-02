7b8c2da6606f3648c455697a54150f5c69a20d92
Saptarshi Sau <saptarshisau09@gmail.com>
2026-08-03 02:37:29 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int f(int ind,int prev_ind,vector<int>& nums,int n){
-        if(ind==n)return 0;
-        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-        int take=0;
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind])
-         take= 1+f(ind+1,ind,nums,n);
-        int not_take=0+f(ind+1,prev_ind,nums,n);
-        return dp[ind][prev_ind+1]= max(not_take,take);
-    }
     int lengthOfLIS(vector<int>& nums) {
-      memset(dp, -1, sizeof(dp));
-     int n=nums.size();
-    return f(0,-1,nums,n);
+        int n = nums.size();
+        vector<int> temp;
+        for(int i=0;i<n;i++){
+            if(temp.empty()|| temp.back()<nums[i]){
+                temp.push_back(nums[i]);
+            }else{
+                int ind= lower_bound(temp.begin(),temp.end(),nums[i]) -temp.begin();
+                temp[ind] =nums[i];
+            }
+        }
+        return temp.size();
     }
 };
 
