ab3ef17f7aa663d1ffe4a846ccb279629e993923
Saptarshi Sau <saptarshisau09@gmail.com>
2026-08-08 00:49:55 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 878949-implement-upper-bound-GFG/878949-implement-upper-bound.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int n=arr.size();
+        int l=0,r=n-1;
+        int ans=n;
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            if(arr[mid]>target){
+                ans=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
